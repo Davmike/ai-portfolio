11ec7bc4a298329b95342b8a29f6a7c2959ec784
Davmike <davidmikeladze00@gmail.com>
2024-10-25 00:56:37 +0400

    second fixed commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { MyContext } from "./Components/Context";
 
 
 function App() {
 
   return (
     <>
-
+      <MyContext.Provider
+        value={{
+        }}
+      >
+      </MyContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

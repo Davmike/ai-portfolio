afc808cb29c795b24681caf6e7e50e5d994b4434
Davmike <davidmikeladze00@gmail.com>
2024-11-06 18:33:21 +0400

    feat: add fonts for sign up

--- src/Components/Sign.tsx
+++ src/Components/Sign.tsx
@@ -10,7 +10,7 @@ function Sign() {
 
     return (
         !isSign && (
-            <div className="fixed inset-0 z-10 flex items-center justify-center">
+            <div className="fixed inset-0 z-10 flex items-center justify-center" id="sign">
                 <div className="absolute inset-0 bg-black/20 backdrop-blur-[0.1px] grayscale-[100%]" onClick={() => setIsSign(!isSign)}></div>
 
                 <div className="bg-[#1E222C] text-white rounded-lg shadow-lg w-[400px] max-w-[90%] p-6 relative z-10">
@@ -20,11 +20,11 @@ function Sign() {
                     >
                         &times;
                     </button>
-                    <h2 className="mb-2 text-xl font-semibold">LOGIN TO USE THE ASSISTANT</h2>
-                    <p className="mb-4 text-gray-400">
+                    <h2 className="mb-2 text-[18px] font-semibold" id="login-text">LOGIN TO USE THE ASSISTANT</h2>
+                    <p className="mb-4 text-gray-400 text-[11px]">
                         I really appreciate your interest in this project.
                     </p>
-                    <p className="pl-3 mb-6 text-gray-400 border-l-2 border-blue-500">
+                    <p className="pl-3 mb-6 text-gray-400 border-l-2 border-blue-500 text-[12px]">
                         To interact with the assistant, please sign-in using your Google or GitHub credentials.
                     </p>
                     <div className="flex justify-center gap-4 mt-6">
@@ -35,7 +35,7 @@ function Sign() {
                             <span className="font-semibold">GOOGLE</span>
                         </button>
                     </div>
-                    <p className="mt-4 text-xs text-center text-gray-500">
+                    <p className="mt-4 text-[10px] text-center text-gray-500">
                         Choose a sign-in method
                     </p>
                 </div>
